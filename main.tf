396c902d64f9975c0207581b315c0668a34293c3
AlessandroFazenda <adrfazenda@gmail.com>
2022-10-20 19:33:57 -0300

    sem docker
    remocação de providers não utilizados

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-# Declaring the Required provider (Docker provider)
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "2.16.0"
-    }
-  }
-}
 
 # Specifying the Docker provider configuration
 provider "docker" {
