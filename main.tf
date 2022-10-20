9440d0806127f592b0d1524b45170defd66a73e1
AlessandroFazenda <adrfazenda@gmail.com>
2022-10-20 20:59:17 -0300

    volta o docker
    tem que ter o provider do docker para funcionar

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+# Declaring the Required provider (Docker provider)
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.16.0"
+    }
+  }
+}
 
 # Specifying the Docker provider configuration
 provider "docker" {
